50c2d818a91c94bd2f59b1ff8acdbdf7e2f964cf
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2023-01-25 19:28:54 +0100

    Spring project set up "Hello World"

---
+++ recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
@@ -0,0 +1,14 @@
+package com.iv1201.recapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecAppApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
