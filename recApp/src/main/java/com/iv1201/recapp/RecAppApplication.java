97d1b4f7fa54cad3b8d3971b71fd68845f469bab
Parosh Shaways <shaways@kth.se>
2023-01-27 16:23:58 +0100

    gt

--- recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
+++ recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
@@ -8,7 +8,7 @@ public class RecAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecAppApplication.class, args);
-		System.out.println("Hello FT");
+		System.out.println("Hello GT");
 	}
 
 }
