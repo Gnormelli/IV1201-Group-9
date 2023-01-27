7cbe0ad23808bcb701dc64c4626718058bb99a36
ftajik <ftajik@kth.se>
2023-01-27 14:10:59 +0100

    hello world

--- recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
+++ recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
@@ -8,7 +8,7 @@ public class RecAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecAppApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello World!!!");
 	}
 
 }
