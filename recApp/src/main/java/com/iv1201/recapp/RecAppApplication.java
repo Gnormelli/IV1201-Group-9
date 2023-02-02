6579a50f2788214bb965fea3fd636a63b436aaea
Parosh Shaways <shaways@kth.se>
2023-02-02 15:26:30 +0100

    react app

--- recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
+++ recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
@@ -8,7 +8,7 @@ public class RecAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecAppApplication.class, args);
-		System.out.println("Hello GT");
+		System.out.println("Hello BD");
 	}
 
 }
