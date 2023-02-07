7575e92f6fdeca8594993589bdbb99a29df0f4d8
Gnormelli <70812864+Gnormelli@users.noreply.github.com>
2023-02-07 13:15:12 +0100

    Update SecurityConfiguration.java

--- recApp/src/main/java/com/iv1201/recapp/Config/SecurityConfiguration.java
+++ recApp/src/main/java/com/iv1201/recapp/Config/SecurityConfiguration.java
@@ -14,6 +14,9 @@ import java.util.Arrays;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+/**
+ * @Author Gustav Normelli
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
@@ -26,7 +29,7 @@ public class SecurityConfiguration {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin(withDefaults());
         return http.build();
     }
     @Bean
