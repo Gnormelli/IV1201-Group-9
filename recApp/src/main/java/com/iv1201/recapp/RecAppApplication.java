1ad13ba500c81d6e888ee53d588acee640d88726
ftajik <ftajik@kth.se>
2023-02-07 13:21:10 +0100

    dd

--- recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
+++ recApp/src/main/java/com/iv1201/recapp/RecAppApplication.java
@@ -8,7 +8,7 @@ public class RecAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RecAppApplication.class, args);
-		System.out.println("Hello Parosh");
+		System.out.println("Hello Gustav");
 	}
 
 }
