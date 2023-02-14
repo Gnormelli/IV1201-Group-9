27ba891f351186a3ca56a5232649a2487b6e102f
Parosh Shaways <shaways@kth.se>
2023-02-14 13:41:41 +0100

    Admin Component

--- recApp/src/main/java/com/iv1201/recapp/Controller/AuthController.java
+++ recApp/src/main/java/com/iv1201/recapp/Controller/AuthController.java
@@ -6,23 +6,20 @@ import com.iv1201.recapp.Models.auth.RegisterRequest;
 import com.iv1201.recapp.Service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     @Autowired
     private AuthService authService;
-    @PostMapping("/register")
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> register(
             @RequestBody RegisterRequest registerRequest
     ){
         return ResponseEntity.ok(authService.register(registerRequest));
     }
-    @PostMapping("/authenticate")
+    @PostMapping("/login")
     public ResponseEntity<AuthResponse> authenticate(
             @RequestBody AuthRequest authRequest
     ){
